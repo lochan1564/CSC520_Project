43c6aa59c75e675187249551d3d96d62247de868
bjlayko <bjlaayko@ncsu.edu>
2021-05-03 13:44:32 -0400

    Update to main and env

--- Environment.py
+++ Environment.py
@@ -1,6 +1,7 @@
 from Tiles import Tiles
+import pandas as pd
+import numpy as np
 import gen_random_env
-import test_tiles
 class Environment:
     def __init__(self, random, fileName):
         self.TileList = []
@@ -13,10 +14,38 @@ class Environment:
         self.TileList = gen_random_env.gen_smoothed_random_env()
     
     def importTileFile(self, fileName):
-        self.TileList = test_tiles.importFromFile(fileName)
+        self.TileList = importFromFile(fileName)
     
     def getTile(self, x, y):
         if(x >= 0 and x < 15 and y >= 0 and y < 15):
             return self.TileList[y][x]
         else:
             return None
+    
+    def importFromFile(tileFile):
+        filepath = tileFile
+
+        board_summary = pd.read_csv(filepath)
+
+        # List to store tile objects
+        tile_list = list()
+
+        for i in range(len(board_summary)):
+            x = board_summary['x'][i]
+            y = board_summary['y'][i]
+            t_id = board_summary['id'][i]
+            tile = Tiles(x,y,t_id)
+            tile_list.append(tile)
+    
+ 
+
+        board = [[None for _ in range(15)] for _ in range(15)]
+
+        # board = np.full(tuple([15]*2),"   ")
+
+        for i in range(15):
+            for j in range(15):
+                board[i][j] = tile_list[i*15+j]
+                #board[i][j] = tile_list[i*15+j].tile_name
+
+        return board
--- main.py
+++ main.py
@@ -14,7 +14,29 @@ else:
 
 uniform = ucs.uniform_cost_search_roads_built(env)
 print(uniform)
+envCost = 0 
+ecoCost = 0
+socCost = 0
+for tileCoord in uniform:
+    costs = env.getTile(tileCoord[0], tileCoord[1]).getCost()
+    ecoCost += costs[0]
+    socCost += costs[1]
+    envCost += costs[2]
+print("Economic cost: "+ str(ecoCost))
+print("Social cost: "+ str(socCost))
+print("Environmental cost: "+ str(envCost))
 
 astar = astar.a_star_search_roads_built(env)
 print(astar)
 
+envCost = 0 
+ecoCost = 0
+socCost = 0
+for tileCoord in uniform:
+    costs = env.getTile(tileCoord[0], tileCoord[1]).getCost()
+    ecoCost += costs[0]
+    socCost += costs[1]
+    envCost += costs[2]
+print("Economic cost: "+ str(ecoCost))
+print("Social cost: "+ str(socCost))
+print("Environmental cost: "+ str(envCost))
